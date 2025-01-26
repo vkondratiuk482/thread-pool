807f792e9eb3ecddb22315ac1603aee22a85fb11
mokuteki225 <kondratiuk011@gmail.com>
2025-01-26 19:04:39 +0200

    feat: spawn / worker implementation

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,20 @@
 const std = @import("std");
+const ThreadPool = @import("thread-pool.zig").ThreadPool;
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+fn log() void {
+    std.debug.print("Hello from the thread\n", .{});
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var threadPool = try ThreadPool.init(.{ .allocator = allocator, .threads_count = 10 });
+    defer threadPool.deinit();
+
+    try threadPool.spawn(log);
+    try threadPool.spawn(log);
+    try threadPool.spawn(log);
+
+    std.time.sleep(2000 * std.time.ns_per_ms);
 }
--- src/thread-pool.zig
+++ src/thread-pool.zig
@@ -1,51 +1,90 @@
 const std = @import("std");
 
-const Task = *const fn (str: []const u8) void;
+const Task = *const fn () void;
 
 const Options = struct {
     threads_count: ?u8 = null,
     allocator: std.mem.Allocator,
 };
 
-const optimal_thread_count = @max(1, std.Thread.getCpuCount() catch 1);
-
 pub const ThreadPool = struct {
+    stopped: bool,
     threads: []std.Thread,
     mutex: std.Thread.Mutex,
     allocator: std.mem.Allocator,
     condition: std.Thread.Condition,
-    task_queue: std.SinglyLinkedList(Task),
+    task_queue: std.DoublyLinkedList(Task),
 
     const Self = @This();
 
     // we need to init the pool with undefined
     // and then call the constructor passing the reference
     // so that we can pass the pointer to the pool while spawning threads
-    pub fn init(pool: *Self, options: Options) ThreadPool {
-        const thread_count = options.threads_count orelse optimal_thread_count;
+    pub fn init(options: Options) !*ThreadPool {
+        const thread_count = options.threads_count orelse optimalThreadCount();
 
         const threads = try options.allocator.alloc(std.Thread, thread_count);
         errdefer options.allocator.free(threads);
 
-        for (0..thread_count) |i| {
-            threads[i] = std.Thread.spawn(.{}, worker, .{pool});
-        }
+        const pool = try options.allocator.create(ThreadPool);
+        errdefer options.allocator.destroy(pool);
 
-        return ThreadPool{
+        pool.* = ThreadPool{
             .threads = threads,
             .mutex = .{},
             .condition = .{},
             .task_queue = .{},
+            .stopped = false,
             .allocator = options.allocator,
         };
+
+        for (0..thread_count) |i| {
+            threads[i] = try std.Thread.spawn(.{}, worker, .{pool});
+        }
+
+        return pool;
     }
 
-    pub fn deinit(self: *Self) void  {
+    pub fn spawn(self: *Self, task: Task) !void {
+        const node = try self.allocator.create(std.DoublyLinkedList(Task).Node);
+        node.*.data = task;
+
+        self.mutex.lock();
+        self.task_queue.append(node);
+        self.mutex.unlock();
+
+        self.condition.signal();
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.stopped = true;
+        self.condition.broadcast();
+
         for (self.threads) |thread| {
             thread.join();
         }
         self.allocator.free(self.threads);
     }
 
-    fn worker(self: *Self) void {}
+    fn worker(self: *Self) void {
+        while (true) {
+            if (self.stopped) {
+                break;
+            }
+
+            self.mutex.lock();
+            defer self.mutex.unlock();
+
+            while (self.task_queue.popFirst()) |unwrapped| {
+                unwrapped.data();
+                self.allocator.destroy(unwrapped);
+            }
+
+            self.condition.wait(&self.mutex);
+        }
+    }
+
+    fn optimalThreadCount() u64 {
+        return @max(1, std.Thread.getCpuCount() catch 1);
+    }
 };
